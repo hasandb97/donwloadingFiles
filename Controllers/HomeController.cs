0f2df5916979dd7e091d2417f2584a5c591d77b2
h.ansari <h.ansari@H_ANSARI>
2023-07-31 09:54:26 +0430

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult MyFiles()
+        {
+            string mypath = ControllerContext.HttpContext.Server.MapPath("~/Farahmand");
+            DirectoryInfo dirInfo = new DirectoryInfo(mypath);
+            List<FileInfo> files = dirInfo.GetFiles().ToList();
+            return View(files);
+        }
+
+
+        public FileResult DownloadFile( string filename)
+        {
+            string path = Server.MapPath("~/Farahmand/") + filename;
+            //read the file to byte array
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            // send file to download
+            return File(bytes, "application/octet-stream", filename);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
